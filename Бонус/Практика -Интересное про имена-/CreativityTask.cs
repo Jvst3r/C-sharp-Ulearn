ff737b4337e3e8ae0aab8750e3155c85571516f7
Jvst3r <arsunik07@mail.ru>
2025-05-28 23:29:10 +0500

    Допы

---
+++ Бонус/Практика -Интересное про имена-/CreativityTask.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Names
+{
+    // зде
+    internal class CreativityTask
+    {
+        public static HistogramData GetBirthsPerYearHistogram(NameData[] names, string name)
+        {
+            //данные в тексте даны за XX век, то есть с 1900 по 1999 (2000 год исключен, хотя это тоже еще XX век)
+            var birthdayCountPerYear = new double[100]; //массив счета дней рождений по годам ( 0 элемент-1900 год, 99 элемент - 1999 год)
+            var years = new string[100];
+            for (int i = 0;i < years.Length;i++) //заполняем массив years годами XX века
+                years[i] = (1900 + i).ToString();
+           
+            foreach (var nameOfHuman in names)
+            {
+                if (nameOfHuman.Name == name) //считаем дни рождения
+                    birthdayCountPerYear[nameOfHuman.BirthDate.Year - 1900] += 1;
+            }
+            if (name == "михаил")
+                return new HistogramData(string.Format("Рождаемость людей с именем " + name + " по годам \n " +
+                    "(для просмотра года " + "наведите курсор на график и зажмите левую кнопку мыши) \n" +
+                "1945 - Михаил возвдвиг флаг над Рейхстагом(тренд на повышение) \n" +
+                "1985-1991 - Правление М.Горбачева и развал СССР (резкий тренд на понижение)"), years, birthdayCountPerYear);
+            if (name == "юрий")
+                return new HistogramData(string.Format("Рождаемость людей с именем " + name + " по годам \n " +
+                    "(для просмотра года наведите курсор на график и зажмите левую кнопку мыши) \n" +
+                "1961 - Юрий Гагарин летит в космос (рождаемость детей с его именем тоже)"),years, birthdayCountPerYear);
+            if (name == "борис")
+                return new HistogramData(string.Format("Рождаемость людей с именем " + name + " по годам \n " +
+                    "(для просмотра года наведите курсор на график и зажмите левую кнопку мыши) \n" +
+                "90e - выпускник УПИ(ныне УрФУ) уничтожил своей политикой не только экономику, \n" +
+                "но и рождаемость детей с его именем :)"), years, birthdayCountPerYear);
+            if (name == "владимир")
+                return new HistogramData(string.Format("Рождаемость людей с именем " + name + " по годам \n " +
+                    "(для просмотра года наведите курсор на график и зажмите левую кнопку мыши) \n" +
+                "1952 - год рождения В.В.Путина (50e-60e пик рождаемости с этим именем) \n" +
+                "честно, я пытался найти закономерность, но не смог \n" +
+                "ну разве что имя Владимир стало популярно из-за анекдотов про 'Вовочек' :)"), years, birthdayCountPerYear);
+            if (name == "арсений")
+                return new HistogramData(string.Format("Рождаемость людей с именем " + name + " по годам \n " +
+                    "(для просмотра года наведите курсор на график и зажмите левую кнопку мыши) \n" +
+                "Ну ради интереса я посмотрел моё имя,сейчас Арсениев гораздо больше стало ((( "), years, birthdayCountPerYear);
+
+            return new HistogramData(string.Format("Рождаемость людей с именем " + name + " по годам \n " +
+                    "(для просмотра года " + "наведите курсор на график и зажмите левую кнопку мыши)"),years, birthdayCountPerYear);
+        }
+    }
+}
---
+++ Бонус/Практика -Интересное про имена-/HeatmapTask.cs
@@ -0,0 +1,38 @@
+﻿namespace Names;
+
+internal static class HeatmapTask
+{
+    public static double[,] GetStatisticForHeatMap(NameData[] names, double[,] BirthdayForHeatMap)
+    {
+        for (int i = 0; i < 30; i++) //считаем по дням
+        {
+            var oneOfMonths = new double[12];
+
+            foreach (var nameOfHuman in names)
+            {
+                if (nameOfHuman.BirthDate.Day == i + 2)
+                    oneOfMonths[nameOfHuman.BirthDate.Month - 1] += 1; //считаем данные для массива
+            }
+
+            for (int j = 0; j < 12; j++)
+                BirthdayForHeatMap[i, j] = oneOfMonths[j]; //переносим в массив данные за месяц по дням
+        }
+        return BirthdayForHeatMap;
+    }
+
+    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+    {
+        var days = new string[30];// с 2 по 31 числа - 30 дней
+        var months = new string[12]; // 12 месяцев
+        var BirthdayForHeatMap = new double[30, 12]; //массив для данных
+
+        for (int i = 0; i < days.Length; i++) //заполняем дни месяца
+            days[i] = (i + 2).ToString();
+        for (int i = 0; i < months.Length; i++) // заполняем месяцы (по ТЗ числами!)
+            months[i] = (i + 1).ToString();
+
+        BirthdayForHeatMap = GetStatisticForHeatMap(names, BirthdayForHeatMap);
+
+        return new HeatmapData("Карта интенсивностей", BirthdayForHeatMap, days, months); // делаем тот самый heatmap
+    }
+}
